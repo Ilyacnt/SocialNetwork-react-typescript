3e3b181da7b5afec0928988eef1ff90aac11506d
Ilyacnt <ilyagt2012@gmail.com>
2022-01-24 22:47:29 +0300

    Created ui-kit button

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import Header from './header/Header';
+import Sidebar from './sidebar/Sidebar';
+
+const Layout:FC = ({children}) => {
+  return <>
+      <Header/>
+      <div>
+          <Sidebar/>
+          {children}      
+      </div>
+  </>;
+};
+
+export default Layout;
---
+++ src/components/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../../../ui/button/Button';
+
+const Home = () => {
+  return <div>
+      Home
+      <Button>Press Me</Button>
+      </div>;
+};
+
+export default Home;
